170f80d2dacb8d9a1decd159ac0251dca2588cfa
Sam Rogers <james.s.rogers23@gmail.com>
2026-05-05 21:23:32 +0100

    ci@ fixing builds

--- bindings/python/src/lib.rs
+++ bindings/python/src/lib.rs
@@ -3,14 +3,16 @@ use pyo3::prelude::*;
 
 #[pyfunction]
 fn validate_yaml(input: &str) -> PyResult<String> {
-    let doc = parse_yaml_str(input).map_err(|err| pyo3::exceptions::PyValueError::new_err(err.to_string()))?;
+    let doc = parse_yaml_str(input)
+        .map_err(|err| pyo3::exceptions::PyValueError::new_err(err.to_string()))?;
     serde_json::to_string(&validate_document(&doc))
         .map_err(|err| pyo3::exceptions::PyRuntimeError::new_err(err.to_string()))
 }
 
 #[pyfunction]
 fn yaml_to_agsi_json(input: &str) -> PyResult<String> {
-    let doc = parse_yaml_str(input).map_err(|err| pyo3::exceptions::PyValueError::new_err(err.to_string()))?;
+    let doc = parse_yaml_str(input)
+        .map_err(|err| pyo3::exceptions::PyValueError::new_err(err.to_string()))?;
     serde_json::to_string(&ground_model_to_agsi_value(&doc))
         .map_err(|err| pyo3::exceptions::PyRuntimeError::new_err(err.to_string()))
 }
